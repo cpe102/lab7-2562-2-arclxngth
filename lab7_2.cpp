4337d702ad45451447fbc6082d83da75c20fbd71
arclxngth <59277471+arclxngth@users.noreply.github.com>
2020-01-21 15:43:42 +0700

    version 1

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,9 +3,18 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(float score){
+	
+	if(score > 90) return 'A';
+	if(score > 75) return 'B';
+	if(score > 60) return 'C';
+	if(score > 45) return 'D';
+	if(score <=45 ) return 'F';
+
+}
 
 int main(){
+
 	//Input the number of students
 	int N,i = 0;
 	cout << "Enter the number of students: ";
@@ -17,8 +26,10 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
+		getline(cin,name[i]);
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
 		cout << "Score of student " << i+1 << ": ";
+		cin>>score[i];
 		//[Missing Code 3] Get score of the i-th students.
 		i++;
 	}
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,58 @@
-//status = "ONE-NIGHT-STAND";
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age,height,asset;
+
+    cout<<"Please enter age : ";
+    cin>>age;
+
+    if(age < 20){
+
+        cout<<"Please in put height : ";
+        cin>>height;
+
+        if(height < 160)
+
+            cout<<"status = UNFRIEND";
+        
+        else if(height <170)
+
+            cout<<"status = FRIEND";
+        
+        else{
+
+            cout<<"Please input your asset : ";
+            cin>>asset;
+
+            if(asset > 69000000)
+
+                cout<<"status = MARRIED";
+
+            else
+
+                cout<<"status = ONE-NIGHT-STAND";
+            
+        }
+    }
+    else{
+
+        if(age < 30){
+
+            cout<<"Please input your asset : ";
+            cin>>asset;
+
+            if(asset > 10000000)
+
+                cout<<"status = BEST FRIEND";
+
+            else
+            
+                cout<<"status = UNFRIEND";
+        }
+        else
+            cout<<"status = UNFRIEND";
+    }
+}
+
